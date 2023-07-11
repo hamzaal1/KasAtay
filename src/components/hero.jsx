2ceefa1b4c0d24c75077710f4dcd3167746a5fce
D.hamza <hamzademnati21@gmail.com>
2023-07-12 00:45:03 +0200

    add scrollbar and some animation

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,7 +2,7 @@
 import { motion } from 'framer-motion';
 import "./css/hero.css"
 function Hero() {
-    
+
     return (
         <>
             <div className='hero_section min-w-full container mx-auto md:ps-14 py-44'>
@@ -40,19 +40,27 @@ function Hero() {
             <div className='bg-myellow min-w-full container mx-auto md:px-16 py-20 grid grid-cols-1 md:grid-cols-2 '>
                 <h2 className='text-2xl mb-3'>Join the Tea Party</h2>
                 <p>
-                    Embark on a flavorful journey and discover the secrets of Moroccan tea culture through the KasAtay Blog and our engaging podcast episodes.
-                </p>
+                    Welcome to KasAtay, your gateway to the enchanting world of Moroccan tea culture! Our vibrant blog takes you on a flavorful journey, unraveling the secrets behind the art of brewing traditional Moroccan mint tea. Dive into our engaging podcast episodes, where we delve deeper into the history, rituals, and fascinating stories surrounding this iconic beverage. Immerse yourself in the aromatic blends, savor the rich traditions, and learn the techniques passed down through generations. Join us as we explore the magic of Moroccan tea, inviting you to experience a taste of Morocco's hospitality and warmth from wherever you are in the world.                </p>
             </div>
-            <div className='bg-mred min-w-full container mx-auto md:px-16 py-20 grid grid-cols-1 md:grid-cols-2 gap-9 md:gap-32'>
-                <div className='py-20 bg-black flex flex-col justify-center items-center text-7xl  text-mgreen'>
+            <div
+                className='bg-mred min-w-full container mx-auto md:px-16 py-20 grid grid-cols-1 md:grid-cols-2 gap-9 md:gap-32'>
+                <motion.div
+                    initial={{ scale:0.8,y: 30, opacity: 0 }}
+                    whileInView={{scale:1, y: 0, opacity: 1 }}
+                    transition={{ duration: 0.8, ease: "easeOut" }}
+                    className='py-20 bg-black flex flex-col justify-center items-center text-7xl  text-mgreen'>
                     <i className="fa-solid fa-arrow-trend-up -rotate-12 mb-9"></i>
                     <p>100+</p>
-                </div>
-                <div className='py-20 bg-black flex flex-col justify-center items-center text-7xl  text-mgreen'>
+                </motion.div>
+                <motion.div
+                    initial={{ scale:0.8,y: 30, opacity: 0 }}
+                    whileInView={{ scale:1,y: 0, opacity: 1 }}
+                    transition={{ delay: .3, duration: 0.8, ease: "easeOut" }}
+                    className='py-20 bg-black flex flex-col justify-center items-center text-7xl  text-mgreen'>
                     <i className="fa-solid fa-microphone mb-9"></i>
                     <p>50+</p>
-                </div>
-            </div>
+                </motion.div>
+            </div >
         </>
     )
 }
